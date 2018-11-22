de80c3b9e233ce134acec346dcfed7073e2eca4d
Lucas Ingaramo <l.ingaramo@gmail.com>
2018-11-22 17:45:11 -0300

    Update serializers

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -7,7 +7,9 @@ class MovieSerializer
     to_roman_number(object.release_year)
   end
 
-  has_many :casting, if: Proc.new { |record| record.casting.any? }
-  has_many :directors, if: Proc.new { |record| record.directors.any? }
-  has_many :producers, if: Proc.new { |record| record.producers.any? }
+  has_many :casting, serializer: PersonSerializer, if: Proc.new { |record| record.casting.any? }
+
+  has_many :directors, serializer: PersonSerializer, if: Proc.new { |record| record.directors.any? }
+
+  has_many :producers, serializer: PersonSerializer, if: Proc.new { |record| record.producers.any? }
 end
--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -2,7 +2,7 @@ class PersonSerializer
   include FastJsonapi::ObjectSerializer
   attributes :first_name, :last_name, :alias
 
-  has_many :as_actor, if: Proc.new { |record| record.as_actor.any? }
-  has_many :as_director, if: Proc.new { |record| record.as_director.any? }
-  has_many :as_producer, if: Proc.new { |record| record.as_producer.any? }
+  has_many :as_actor, serializer: MovieSerializer, if: Proc.new { |record| record.as_actor.any? }
+  has_many :as_director, serializer: MovieSerializer, if: Proc.new { |record| record.as_director.any? }
+  has_many :as_producer, serializer: MovieSerializer, if: Proc.new { |record| record.as_producer.any? }
 end
